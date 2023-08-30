c844760024498ed3fd5a7bbc3161f2dce79cd103
Frempong <samankwaa.frempong@gmail.com>
2023-08-30 21:57:33 +0000

    Add Provider wrapper to application

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Provider} from 'react-redux';
 import Splash from './Splash';
 import HomePage from './HomePage';
+import store from '../../redux/store';
 
 const App = () => {
   const normalBool = true;
 
   return (
     <SafeAreaView style={style.safeArea}>
-      <View style={style.appWrapper}>
-        {normalBool ? <HomePage /> : <Splash />}
-      </View>
+      <Provider store={store}>
+        <View style={style.appWrapper}>
+          {normalBool ? <HomePage /> : <Splash />}
+        </View>
+      </Provider>
     </SafeAreaView>
   );
 };
@@ -24,4 +28,5 @@ const style = StyleSheet.create({
     flex: 1,
   },
 });
+
 export default App;
