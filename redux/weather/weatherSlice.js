ebb22c74392ae20113d263141a498631901f6a1f
Frempong <samankwaa.frempong@gmail.com>
2023-09-01 19:22:22 +0000

    Create weatherSlice to handle fetching weather data

---
+++ redux/weather/weatherSlice.js
@@ -0,0 +1,36 @@
+const {createSlice, createAsyncThunk} = require('@reduxjs/toolkit');
+
+const initialState = {
+  weatherDetails: {},
+  isLoading: false,
+};
+
+export const fetctWeather = createAsyncThunk(
+  'fetch,fetchWeather',
+  async coord => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coord[0]}&lon=${coord[1]}&appid=cc885dd3877084c87abb1055d2ab813c`;
+    // console.log(url);
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  },
+);
+
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetctWeather.pending, state => ({...state, isLoading: true}))
+      .addCase(fetctWeather.fulfilled, (state, {payload}) => ({
+        ...state,
+        isLoading: false,
+        weatherDetails: payload,
+      }))
+      .addCase(fetctWeather.rejected, state => ({...state, isLoading: false}));
+  },
+});
+
+export default weatherSlice.reducer;
