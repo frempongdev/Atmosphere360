74ed4bdbc791738daca9ba7cffd2e601214e9e2b
Frempong <samankwaa.frempong@gmail.com>
2023-09-01 19:23:09 +0000

    Add weatherSlice reducer to store to handle weatherDetails state globally

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import geolocationReducer from './geolocation/GeolocationSlice';
+import weatherReducer from './weather/weatherSlice';
+
 const store = configureStore({
   reducer: {
     city: geolocationReducer,
+    weather: weatherReducer,
   },
 });
 
